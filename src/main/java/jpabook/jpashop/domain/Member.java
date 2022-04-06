89b767a3ecda557eca123c906f08ef95fde77aa3
YoungKyonYou <man.spride1324@gmail.com>
2022-04-07 00:48:11 +0900

    Member class

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class Member {
+    @Id @GeneratedValue
+    @Column(name="member_id")
+    private Long id;
+
+    private String name;
+
+//    내장 타입임을 선언
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy="member")
+    private List<Order> orders=new ArrayList<>();
+}
