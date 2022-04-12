33cc4f2688f7523cfeb33a3e8a372521e531fcdd
YoungKyonYou <man.spride1324@gmail.com>
2022-04-12 23:41:57 +0900

    update

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+@Data
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member){
+        em.persist(member);
+        //Command와 Query를 분리하기 위해서 Member 객체 대신 member의 id를 반환한다.
+        //저장을 하고 나면 가급적이면 이건 side-effect를 일으키는 command 성이 있어서 아이디 정도만 조회하는 걸로 설계한다.
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+
+}
